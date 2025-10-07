60afedef7e8574d0ccdc7ec9ef31afb4d8d7b5de
wilz0n <fishas02@gmail.com>
2025-10-06 21:11:58 -0500

    feat: 🎸 Ultimo cambio para mejoras de lazyVim

---
+++ lua/plugins/image.lua
@@ -0,0 +1,12 @@
+return {
+  "3rd/image.nvim",
+  opts = {
+    backend = "kitty", -- usa kitty como backend
+    integrations = {
+      markdown = { enabled = true },
+      neorg = { enabled = true },
+    },
+    max_width = 100,
+    max_height = 50,
+  },
+}
